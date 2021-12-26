8e079cea1b6da02668f7707508a3947fa8e4776f
Lokesh <lokeshmghule11g@gmail.com>
2021-12-26 14:49:23 +0530

    designing UI

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -11,8 +11,45 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Welcome'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image(
+                  height: 80.0,
+                  width: 80.0,
+                  image: AssetImage(
+                    'chatting.png',
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Text(
+                'Lets Chat',
+                style: TextStyle(fontSize: 40.0),
+              ),
+            ],
+          ),
+          Material(
+            color: Colors.lightBlueAccent,
+            child: MaterialButton(
+              onPressed: () {},
+              minWidth: 300,
+              height: 40,
+              child: Text(
+                'Login',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            borderRadius: BorderRadius.circular(20),
+          )
+        ],
       ),
     );
   }
